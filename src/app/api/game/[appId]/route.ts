40510f9ad3f306a690b671a303841de74786d660
Sam Buckley <samuel.buckley@hotmail.co.uk>
2024-01-16 09:57:22 +1100

    steam api route added - game

--- src/app/api/game/[appId]/route.ts
+++ src/app/api/game/[appId]/route.ts
@@ -1,24 +1,21 @@
 interface RouteContext {
     params: {
-        steamId: string;
+        appId: string;
     };
 }
 
 export async function GET(request: Request, context: RouteContext) {
     const { params } = context;
-    const steamId = params.steamId; // STEAM_USER_ID_TEST="76561198000549698"
-    const apiKey = process.env.STEAM_API_KEY;
-    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${apiKey}&steamid=${steamId}&format=json`;
+    const appId = params.appId;
+    const url = `https://store.steampowered.com/api/appdetails?appids=${appId}`;
 
-    // Further error handling could be useful here, such as when invalid IDs are passed into the dynamic route segment.
-    // This would also be a good opportunity to write automated tests to match the correct response types and actions to errors.
     try {
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        return new Response(JSON.stringify({ steamId, data: data.response }), {
+        return new Response(JSON.stringify(data[appId].data), {
           headers: {
             "Content-Type": "application/json"
           },
