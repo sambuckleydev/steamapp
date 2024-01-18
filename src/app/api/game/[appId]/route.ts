d76520756a014102dc7a258efd7d25ec53543c01
Sam Buckley <samuel.buckley@hotmail.co.uk>
2024-01-18 20:05:22 +1100

    sass installed / clean up files

--- src/app/api/game/[appId]/route.ts
+++ src/app/api/game/[appId]/route.ts
@@ -1,32 +1,32 @@
 interface RouteContext {
-    params: {
-        appId: string;
-    };
+  params: {
+    appId: string;
+  };
 }
 
 export async function GET(request: Request, context: RouteContext) {
-    const { params } = context;
-    const appId = params.appId;
-    const url = `https://store.steampowered.com/api/appdetails?appids=${appId}`;
+  const { params } = context;
+  const appId = params.appId;
+  const url = `https://store.steampowered.com/api/appdetails?appids=${appId}`;
 
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        return new Response(JSON.stringify(data[appId].data), {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          status: 200
-        });
-      } catch (error: any) {
-        return new Response(JSON.stringify({ error: error.message }), {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          status: 500
-        });
-      }
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return new Response(JSON.stringify(data[appId].data), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 200,
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
+  }
 }
--- src/app/api/user/[steamId]/route.ts
+++ src/app/api/user/[steamId]/route.ts
@@ -1,35 +1,40 @@
 interface RouteContext {
-    params: {
-        steamId: string;
-    };
+  params: {
+    steamId: string;
+  };
 }
 
 export async function GET(request: Request, context: RouteContext) {
-    const { params } = context;
-    const steamId = params.steamId; // STEAM_USER_ID
-    const apiKey = process.env.STEAM_API_KEY;
-    const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${apiKey}&steamid=${steamId}?include_appinfo=true&include_extended_appinfo=true&format=json`;
+  const steamId = context.params.steamId; // STEAM_USER_ID
+  const urlParams = new URLSearchParams({
+    steamid: steamId,
+    key: `${process.env.STEAM_API_KEY}`,
+    include_appinfo: "true",
+    include_extended_appinfo: "true",
+    format: "json",
+  });
+  const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?${urlParams}`;
 
-    // Further error handling could be useful here, such as when invalid IDs are passed into the dynamic route segment.
-    // This would also be a good opportunity to write automated tests to match the correct response types and actions to errors.
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        return new Response(JSON.stringify({ steamId, data: data.response }), {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          status: 200
-        });
-      } catch (error: any) {
-        return new Response(JSON.stringify({ error: error.message }), {
-          headers: {
-            "Content-Type": "application/json"
-          },
-          status: 500
-        });
-      }
+  // Further error handling could be useful here, such as when invalid IDs are passed into the dynamic route segment.
+  // This would also be a good opportunity to write automated tests to match the correct response types and actions to errors.
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return new Response(JSON.stringify({ steamId, data: data.response }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 200,
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
+  }
 }
